e5d1a39d54c5ec40c7384b7775c13d2a01bf4dd3
Jhockinn <Tobiln@hotmail.dk>
2025-04-07 11:34:12 +0200

    test

---
+++ src/main/java/web/peasantgaming/dto/Info.java
@@ -0,0 +1,72 @@
+
+package web.peasantgaming.dto;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.annotation.processing.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "title",
+    "steamAppID",
+    "thumb"
+})
+@Generated("jsonschema2pojo")
+public class Info {
+
+    @JsonProperty("title")
+    private String title;
+    @JsonProperty("steamAppID")
+    private String steamAppID;
+    @JsonProperty("thumb")
+    private String thumb;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    @JsonProperty("title")
+    public String getTitle() {
+        return title;
+    }
+
+    @JsonProperty("title")
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @JsonProperty("steamAppID")
+    public String getSteamAppID() {
+        return steamAppID;
+    }
+
+    @JsonProperty("steamAppID")
+    public void setSteamAppID(String steamAppID) {
+        this.steamAppID = steamAppID;
+    }
+
+    @JsonProperty("thumb")
+    public String getThumb() {
+        return thumb;
+    }
+
+    @JsonProperty("thumb")
+    public void setThumb(String thumb) {
+        this.thumb = thumb;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
---
+++ src/main/java/web/peasantgaming/service/CheapSharkService.java
@@ -0,0 +1,29 @@
+package web.peasantgaming.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+public class CheapSharkService {
+
+    private final WebClient webClient;
+
+    @Autowired
+    public CheapSharkService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build()   ;
+    }
+
+    public Mono<String> fetchFromCheapShark(Integer id){
+        String Url = "https://www.cheapshark.com/api/1.0/games?id=" + id + "?limit=5";
+
+        return webClient.get()
+                .uri(Url)
+                .retrieve()
+                .bodyToMono(String.class);
+
+    }
+
+}
