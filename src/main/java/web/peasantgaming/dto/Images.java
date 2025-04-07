ca29d4151fb793d5ab288bc34d3983e9d4bc5360
Jhockinn <Tobiln@hotmail.dk>
2025-04-07 11:44:28 +0200

    test

---
+++ src/main/java/web/peasantgaming/dto/Images.java
@@ -0,0 +1,72 @@
+
+package web.peasantgaming.dto;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.annotation.processing.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "banner",
+    "logo",
+    "icon"
+})
+@Generated("jsonschema2pojo")
+public class Images {
+
+    @JsonProperty("banner")
+    private String banner;
+    @JsonProperty("logo")
+    private String logo;
+    @JsonProperty("icon")
+    private String icon;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    @JsonProperty("banner")
+    public String getBanner() {
+        return banner;
+    }
+
+    @JsonProperty("banner")
+    public void setBanner(String banner) {
+        this.banner = banner;
+    }
+
+    @JsonProperty("logo")
+    public String getLogo() {
+        return logo;
+    }
+
+    @JsonProperty("logo")
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    @JsonProperty("icon")
+    public String getIcon() {
+        return icon;
+    }
+
+    @JsonProperty("icon")
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
---
+++ src/main/java/web/peasantgaming/dto/StoreInfo.java
@@ -0,0 +1,85 @@
+
+package web.peasantgaming.dto;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.annotation.processing.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "storeID",
+    "storeName",
+    "isActive",
+    "images"
+})
+@Generated("jsonschema2pojo")
+public class StoreInfo {
+
+    @JsonProperty("storeID")
+    private String storeID;
+    @JsonProperty("storeName")
+    private String storeName;
+    @JsonProperty("isActive")
+    private Integer isActive;
+    @JsonProperty("images")
+    private Images images;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    @JsonProperty("storeID")
+    public String getStoreID() {
+        return storeID;
+    }
+
+    @JsonProperty("storeID")
+    public void setStoreID(String storeID) {
+        this.storeID = storeID;
+    }
+
+    @JsonProperty("storeName")
+    public String getStoreName() {
+        return storeName;
+    }
+
+    @JsonProperty("storeName")
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    @JsonProperty("isActive")
+    public Integer getIsActive() {
+        return isActive;
+    }
+
+    @JsonProperty("isActive")
+    public void setIsActive(Integer isActive) {
+        this.isActive = isActive;
+    }
+
+    @JsonProperty("images")
+    public Images getImages() {
+        return images;
+    }
+
+    @JsonProperty("images")
+    public void setImages(Images images) {
+        this.images = images;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
